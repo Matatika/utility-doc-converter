0aac455b74c5956264d93809d791278d8b241436
Reuben Frankel <rfrankel@matatika.com>
2023-11-22 10:58:39 +0000

    Support OCR to extract text from PDF scans

--- doc_converter/utils.py
+++ doc_converter/utils.py
@@ -1,8 +1,10 @@
+import os
 import tempfile
 from pathlib import Path
 
 import click
 import mammoth
+import ocrmypdf
 import pdf2docx
 import yaml
 from matatika.dataset import DatasetV0_2
@@ -18,9 +20,29 @@ def is_supported_file(file: Path):
     return file.suffix in SUPPORTED_FILE_TYPES
 
 
-def convert_docx_to_md(docx: Path):
+def convert_docx_to_description(docx: Path):
     with docx.open("rb") as f:
-        return mammoth.convert_to_markdown(f)
+        value: str = mammoth.convert_to_markdown(f).value
+        return value.replace("\\#", "#").strip()
+
+
+def convert_pdf_to_description(pdf: Path):
+    with tempfile.TemporaryDirectory(prefix="doc-converter-") as tmp_dir:
+        tmp_docx = Path(tmp_dir) / f"{pdf.stem}.docx"
+
+        pdf_converter = pdf2docx.Converter(str(pdf))
+        pdf_converter.convert(str(tmp_docx))
+        pdf_converter.close()
+
+        md = convert_docx_to_description(tmp_docx)
+
+        if md:
+            return md
+
+        tmp_txt = tmp_docx.with_suffix(".txt")
+        ocrmypdf.ocr(pdf, os.devnull, sidecar=tmp_txt)
+
+        return tmp_txt.read_text().strip()
 
 
 def convert_to_dataset(file: Path):
@@ -31,22 +53,13 @@ def convert_to_dataset(file: Path):
     click.echo(f"Converting {file}...")
 
     if file.suffix == ".pdf":
-        with tempfile.TemporaryDirectory(prefix="doc-converter-") as tmp_dir:
-            tmp_docx = Path(tmp_dir) / f"{file.stem}.docx"
-
-            pdf_converter = pdf2docx.Converter(str(file))
-            pdf_converter.convert(str(tmp_docx))
-            pdf_converter.close()
-
-            result = convert_docx_to_md(tmp_docx)
+        description = convert_pdf_to_description(file)
     else:
-        result = convert_docx_to_md(file)
-
-    markdown = result.value.replace("\\#", "#")
+        description = convert_docx_to_description(file)
 
     dataset = DatasetV0_2()
     dataset.title = file.stem
-    dataset.description = markdown
+    dataset.description = description
 
     dataset_yml = file.parent / f"{file.stem}.yml"
 
