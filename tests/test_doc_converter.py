c8edabd5dc9afecac9a36c0342b305b6f0ac6c41
Reuben Frankel <rfrankel@matatika.com>
2024-04-03 17:33:49 +0100

    Add more tests with sample documents

--- tests/test_doc_converter.py
+++ tests/test_doc_converter.py
@@ -1,5 +1,6 @@
 """Unittest module for doc-converter utility"""
 
+import shutil
 import tempfile
 import unittest
 from pathlib import Path
@@ -16,6 +17,71 @@ class TestDocConverter(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_convert_file_docx(self):
+        with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
+            shutil.copy("tests/sample_documents/Aircraft leasing.docx", tmp_dir)
+
+            result = self.runner.invoke(convert, tmp_dir)
+
+            self.assertIs(result.exit_code, 0)
+
+            tmp_dir = Path(tmp_dir)
+
+            self.assertIs((tmp_dir / "Aircraft leasing.yml").exists(), True)
+
+    def test_convert_file_pdf(self):
+        with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
+            shutil.copy("tests/sample_documents/Lorem Ipsum.pdf", tmp_dir)
+
+            result = self.runner.invoke(convert, tmp_dir)
+
+            self.assertIs(result.exit_code, 0)
+
+            tmp_dir = Path(tmp_dir)
+
+            self.assertIs((tmp_dir / "Lorem Ipsum.yml").exists(), True)
+
+    def test_convert_file_pdf_ocr(self):
+        with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
+            shutil.copy("tests/sample_documents/Facsimile transmission.pdf", tmp_dir)
+
+            result = self.runner.invoke(convert, tmp_dir)
+
+            self.assertIs(result.exit_code, 0)
+
+            tmp_dir = Path(tmp_dir)
+
+            self.assertIs((tmp_dir / "Facsimile transmission.yml").exists(), True)
+
+    def test_convert_dir(self):
+        with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
+            shutil.copytree("tests/sample_documents", tmp_dir, dirs_exist_ok=True)
+
+            result = self.runner.invoke(convert, tmp_dir)
+
+            self.assertIs(result.exit_code, 0)
+
+            tmp_dir = Path(tmp_dir)
+
+            self.assertTrue((tmp_dir / "Aircraft leasing.yml").exists())
+            self.assertTrue((tmp_dir / "Facsimile transmission.yml").exists())
+            self.assertTrue((tmp_dir / "Lorem Ipsum.yml").exists())
+
+    def test_convert_sub_dir(self):
+        with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
+            sub_dir = f"{tmp_dir}/sample_documents"
+            shutil.copytree("tests/sample_documents", sub_dir)
+
+            result = self.runner.invoke(convert, tmp_dir)
+
+            self.assertIs(result.exit_code, 0)
+
+            sub_dir = Path(sub_dir)
+
+            self.assertTrue((sub_dir / "Aircraft leasing.yml").exists())
+            self.assertTrue((sub_dir / "Facsimile transmission.yml").exists())
+            self.assertTrue((sub_dir / "Lorem Ipsum.yml").exists())
 
     def test_convert_empty_dir(self):
         with tempfile.TemporaryDirectory(prefix="test-doc-converter-") as tmp_dir:
